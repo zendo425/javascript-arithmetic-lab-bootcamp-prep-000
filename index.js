c0e88052009234d7f97531ec7332608214d62bde
Demetrious Young <zendo425@gmail.com>
2018-01-08 07:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function subtract(a, b) {
     return a / b;
  }
  
- var increment = (function(n) {
-  return function() 
+ var increment = (function(n) 
+  return function() }
     n += 1;
     return n;
  
